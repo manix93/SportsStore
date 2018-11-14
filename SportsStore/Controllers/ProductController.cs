b31e9b43a092c5ca0ef6a2c870ace256ccf7c121
Lukasz Maciag <manix93@gmail.com>
2018-11-14 14:14:30 +0100

    Add Repository as Service. Add product controller with simple view.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult List() => View(_repository.Products);
+    }
+}
